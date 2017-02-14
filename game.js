765a2ac9418309e5a8052cdfaa2f67376dc1dcda
Clara Monsalve <dcamilo.bedoya@udea.edu.co>
2017-02-14 11:23:44 -0500

    Eventos mouse

--- game.js
+++ game.js
@@ -1,3 +1,4 @@
+
 ;(function(){
   const ctx = document.getElementById("canvas").getContext("2d");
   class Random{
@@ -6,6 +7,7 @@
     }
   }
 
+//Comida de la serpiente
   class Food{
     constructor(x,y){
       this.x = x;
@@ -45,16 +47,19 @@
       ctx.fillStyle = 'green';*/
     }
     static generate(){
-        const x = Random.get(0,600);
-        const y = Random.get(0,400);     
+        const x = Random.get(5,500);
+        const y = Random.get(5,290);     
         return new Food(x,y);
     }
   }
+
+
+  //Clase para dibujar el rectangulo 
   class Square{
     constructor(x,y){
       this.width = 10
       this.height = 10
-      this.speed = 10
+      this.speed = 11
       this.x = x
       this.y = y
       this.backSquare = null
@@ -91,11 +96,12 @@
     }
 
     addNew(){
-      if(this.hasBack()){
-        this.backSquare.addNew()
-      }else{
-        this.backSquare = new Square(this.x - (this.width+5),this.y)
-      }
+
+      if(this.hasBack())
+       this.backSquare.addNew();
+      else
+        this.backSquare = new Square(this.x - (this.width+5),this.y);
+
     }
 
     hasBack(){
@@ -106,7 +112,8 @@
       ctx.arc(this.x+50, this.y+50, 5, 0, 2 * Math.PI);
       ctx.stroke();*/
       ctx.fillRect(this.x,this.y,this.width,this.height);
-      ctx.fillStyle = 'blue';
+      ctx.fillStyle = 'black';
+      ctx.fill();
       if(this.hasBack()) this.backSquare.draw()
     }
 
@@ -129,6 +136,9 @@
     }
 
   }
+
+
+  //Clase correspondiente a la serpiente
   class Snake{
     constructor(){
       this.mainSquare = new Square(0,0)
@@ -193,6 +203,50 @@
   let foods = []
   var allowHorizontalMov = true;
   var allowVerticalMov = true;
+
+  //Posiciones iniciales y finales para controlar el evento de arrastrar el mouse
+  var initialPosX=0;                          
+  var initialPosY=0;
+  var finalPosX=1;
+  var finalPosY=1;
+
+  canvas.addEventListener("mousedown", function (e) {
+        initialPosX=e.clientX;
+        initialPosY=e.clientY;
+        console.log(initialPosX);
+}, false)
+
+  canvas.addEventListener("mouseup", function (e) {
+          finalPosX=e.clientX;
+          finalPosY=e.clientY;
+}, false);
+
+canvas.addEventListener("mousemove", function (e) {
+  if(initialPosX>finalPosX && allowHorizontalMov==true)
+  {
+      allowHorizontalMov=false;
+      allowVerticalMov=true;
+      snake.left();
+  }
+  else if(initialPosX<finalPosX && allowHorizontalMov==true)
+  {
+      allowHorizontalMov=false;
+      allowVerticalMov=true;
+      snake.right();
+  }
+  else if(initialPosY>finalPosY && allowVerticalMov == true)
+  {
+      allowHorizontalMov=true;
+      allowVerticalMov=false;
+      snake.up()
+  }
+  else if(initialPosY < finalPosY && allowVerticalMov == true)
+  {
+      allowHorizontalMov=true;
+      allowVerticalMov=false;
+      snake.down()
+  }
+}, false);
   
   window.addEventListener("keydown",(ev)=>{
     // 39 right
@@ -202,24 +256,28 @@
     ev.preventDefault()
     if(ev.keyCode == 37 && allowHorizontalMov==true)
     {
+      console.log("Me moví a la izq");
       allowHorizontalMov=false;
       allowVerticalMov=true;
       snake.left()
     }
     if(ev.keyCode == 38 && allowVerticalMov == true)
     { 
+      console.log("Me moví hacia arriba");
       allowHorizontalMov=true;
       allowVerticalMov=false;
       snake.up()
     }
     if(ev.keyCode == 39 && allowHorizontalMov==true )
     { 
+      console.log("Me moví a la derecha");
       allowHorizontalMov=false;
       allowVerticalMov=true;
       snake.right()  
     }  
     if(ev.keyCode == 40 && allowVerticalMov == true) 
     {
+      console.log("Me moví hacia abajo");
       allowHorizontalMov=true;
       allowVerticalMov=false;
       snake.down()
@@ -299,6 +357,8 @@
   }
   
 })()
+
+
 
 
 var head = document.getElementById("head");
